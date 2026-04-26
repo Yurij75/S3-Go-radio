7bcfd1d5275a256d04deea60bb1f1db0edd199ac
Yurij75 <yurij75maks@gmail.com>
2026-04-26 14:24:58 +0300

    Add files via upload

---
+++ src/fonts/fonts.h
@@ -0,0 +1,15 @@
+#ifndef PROJECT_FONTS_H
+#define PROJECT_FONTS_H
+
+#include <Arduino_GFX_Library.h>
+
+#include "consolabUkr6.h"
+#include "consolabUkr8.h"
+#include "consolabUkr12.h"
+#include "consolabUkr16.h"
+#include "consolabUkr18.h"
+#include "consolabUkr36.h"
+#include "DirectiveFour40.h"
+
+
+#endif // PROJECT_FONTS_H
