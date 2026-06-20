89c9b9a8ec438a4806254ad12d8af2737f660118
Yurij75 <yurij75maks@gmail.com>
2026-06-20 12:38:50 +0300

    Add files via upload

---
+++ src/fonts/fonts.h
@@ -0,0 +1,16 @@
+#ifndef PROJECT_FONTS_H
+#define PROJECT_FONTS_H
+
+#include <Arduino_GFX_Library.h>
+
+#include "consolabUkr6.h"
+#include "consolabUkr8.h"
+#include "consolabUkr12.h"
+#include "consolabUkr16.h"
+#include "consolabUkr18.h"
+#include "consolabUkr36.h"
+#include "fallout48pt7b.h"
+#include "DirectiveFour40.h"
+#include "Fallout_54px.h"
+#include "ComfortaaBold_24px.h"
+#endif // PROJECT_FONTS_H
